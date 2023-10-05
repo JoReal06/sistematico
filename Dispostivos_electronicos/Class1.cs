fb706c69e87df8c51acde6771a9846e1de0adca1
JADPA-20 <JADPA-20@DESKTOP-JE0L0R3>
2023-10-05 10:12:02 -0600

    Agregar archivos de proyecto.

---
+++ Dispostivos_electronicos/Class1.cs
@@ -0,0 +1,74 @@
+﻿namespace Dispostivos_electronicos
+{
+    public interface ICamara
+    {
+        void MostrarAppCamara();
+        void TomarFoto();
+
+        public string NombreFoto { get; set; }
+        public int CantidadDEFotos { get; set; }
+
+    }
+
+    public class Celular : ICamara
+    {
+        public string NombreFoto { get; set; }
+        public int CantidadDEFotos { get; set; }
+
+        public Celular(string nombrefoto, int cantidadFotos)
+        { 
+            this.NombreFoto = nombrefoto;
+            this.CantidadDEFotos = cantidadFotos;
+        }
+
+        public void MostrarAppCamara()
+        {
+            Console.WriteLine("se esta mostrando algo en pantalla, y la app es......\n");
+        }
+
+        public void TomarFoto()
+        {
+            Console.WriteLine("se tomo foto\n");
+        }
+    }
+
+
+    public interface IGps : ICamara
+    {
+        void localizar();
+        void ObtenerCoordenadas();
+    }
+
+    public class Tablets : IGps
+    {
+        public string NombreFoto { get; set; }
+        public int CantidadDEFotos { get; set; }
+
+        public Tablets(string nombreFoto, int cantidadFotos)
+        { 
+            this.NombreFoto = nombreFoto;
+            this.CantidadDEFotos= cantidadFotos;
+        }
+
+        public void localizar()
+        {
+            Console.WriteLine("se esta localizando el dispostivo\n");
+        }
+
+        public void MostrarAppCamara()
+        {
+            Console.WriteLine("se esta mostrado algo en pantalla, la app es .......\n");
+        }
+
+        public void ObtenerCoordenadas()
+        {
+            Console.WriteLine("Las Coordenadas son: 48o 51’ 30.2328’’N, 2o 17’ 40.1388’’E\n");
+        }
+
+        public void TomarFoto()
+        {
+            Console.WriteLine("se tomo la foto\n");
+        }
+    }
+
+}
---
+++ Sistematico/Program.cs
@@ -0,0 +1,21 @@
+﻿using Dispostivos_electronicos;
+
+Celular samsung = new Celular("foto de la palya", 5);
+Console.WriteLine("El  celular sansumg realizo\n");
+
+samsung.MostrarAppCamara();
+samsung.TomarFoto();
+Console.WriteLine($"El nombre de la foto es: {samsung.NombreFoto} y la cantidad de fotos " +
+    $"que se tomaron son {samsung.CantidadDEFotos}\n");
+
+Tablets blue = new Tablets("Foto en la boda", 10);
+Console.WriteLine("La tablet realizo\n");
+blue.MostrarAppCamara();
+blue.TomarFoto();
+blue.localizar();
+blue.ObtenerCoordenadas();
+
+Console.WriteLine($"El nombre de la fotos es: {blue.NombreFoto} y la cantidad de " +
+    $"fotos que se tomaron son : {blue.CantidadDEFotos}");
+
+
